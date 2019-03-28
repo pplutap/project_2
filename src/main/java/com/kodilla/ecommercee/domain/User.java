6072302e63ad3a852a3b708db12c4f686a746124
Andrzej Jaromin <ajaromin@silenteight.com>
2019-03-28 22:48:47 +0100

    Fixed broken User entity

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -3,9 +3,14 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id
     private Long userId;
     private String userName;
     private String isBlocked;
