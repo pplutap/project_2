8731e1800a96433c63164414f4dbdaed41b9c0ae
124 <123@MacBook-Pro-Karol-2.local>
2019-03-28 17:36:51 +0100

    v3

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -3,9 +3,11 @@ package com.kodilla.ecommercee.controller;
 import com.kodilla.ecommercee.domain.UserDto;
 import com.kodilla.ecommercee.mapper.UserMapper;
 import com.kodilla.ecommercee.service.UserService;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -23,7 +25,8 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getUsers")
     public List<UserDto> getUsers() {
-        return userMapper.mapToUserDtoList(userService.getUsersList());
+//        return userMapper.mapToUserDtoList(userService.getUsersList());
+        return new ArrayList<>();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getUser")
@@ -37,13 +40,13 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
-    public UserDto blockUser(@RequestBody UserDto userDto) {
-        return new UserDto(userDto.getUserId(), userDto.getUserName(), "Blocked", userDto.getUserIdKey());
+    public UserDto blockUser(@RequestParam Long userId, @RequestBody UserDto userDto) {
+        return new UserDto(userId, userDto.getUserName(), "Blocked", userDto.getUserIdKey());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "generateUserIdKey")
     public Long generateUserIdKey(@RequestParam Long userId) throws UserNotFoundException {
         Long randomKey = new Random().nextLong();
-        return randomKey;
+        return userId + randomKey;
     }
 }
