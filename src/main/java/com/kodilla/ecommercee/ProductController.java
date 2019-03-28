4fb472d970fd3be748c039ee0845dc35d9b49fa3
nyuqiu <tomeczek110@gmail.com>
2019-03-28 17:54:56 +0000

    Initial commit

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @GetMapping
+    public List<ProductDto> getProducts(){
+
+    }
+
+    @GetMapping("{id}")
+    public ProductDto getProduct(@PathVariable("id") Long productId) thorws ProductNotFoundException {
+
+    }
+
+    @DeleteMapping("id")
+    public void deleteProduct(@PathVariable("id") Long taskId){
+
+    }
+
+    @PutMapping()
+    public ProductDto updateTask(@RequestBody ProductDto productDto){
+
+    }
+
+    @PostMapping(consumes = "application/json")
+    public void createTask(@RequestBody ProductDto productDto){
+
+    }
 }
