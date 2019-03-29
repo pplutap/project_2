f7c851394402b992c6b2ef010cf3d7999807fe66
124 <123@MacBook-Pro-Karol-2.local>
2019-03-29 16:17:36 +0100

    Created entity for class User

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -3,17 +3,25 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
+
+    @Column(name = "USERNAME")
     private String userName;
+
+    @Column(name = "ISBLOCKED")
     private String isBlocked;
+
+    @Column(name = "USERIDKEY")
     private Long userIdKey;
 }
 
