69b9c1223045f2ab6ee70a7773e626e20c84dafd
karol-piotrowski <kp0@o2.pl>
2019-03-29 16:12:16 +0100

    Task JDP190301-5 - Initial UserController implementation

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,25 @@
 package com.kodilla.ecommercee;
 
+
+import com.kodilla.ecommercee.cart.domain.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("ecommercee/user")
 public class UserController {
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public Long createUser(@RequestBody String userName) {
+        UserDto userDto = new UserDto(1L, userName);
+        return userDto.getUserId();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    public Long blockUser(@RequestParam Long userId) {
+        return userId;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "generateKey")
+    public String generateKey(@RequestParam Long userId) {
+        return "12345678qwertyuiop";
+    }
 }
