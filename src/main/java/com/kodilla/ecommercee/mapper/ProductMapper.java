73828410631d9b6dd8abf8e14803d4495ba1784c
majkel3321 <majkel3321@gmail.com>
2019-03-29 15:35:03 +0100

    Product Mapper

---
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -0,0 +1,31 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ProductMapper {
+
+
+    public Product mapToProduct(ProductDto productDto) {
+        return new Product(productDto.getId(),
+                productDto.getName(),
+                productDto.getPrice());
+    }
+
+    public ProductDto mapToProductDto(Product product){
+        return new ProductDto(product.getId(),
+                product.getName(),
+                product.getPrice());
+    }
+
+    public List<ProductDto> mapToProductDtoList(List<Product> productList){
+        return productList.stream()
+                .map(t -> new ProductDto(t.getId(),t.getName(),t.getPrice()))
+                .collect(Collectors.toList());
+    }
+}
