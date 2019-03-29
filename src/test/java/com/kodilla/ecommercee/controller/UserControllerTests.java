41d9f8d3863fde7e2a780140b85391f3a214cfba
karol-piotrowski <kp0@o2.pl>
2019-03-29 23:38:00 +0100

    Test methods body added for UserController

--- src/test/java/com/kodilla/ecommercee/controller/UserControllerTests.java
+++ src/test/java/com/kodilla/ecommercee/controller/UserControllerTests.java
@@ -1,7 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.cart.domain.UserDto;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,18 +12,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserControllerTests {
 
+    @Autowired
+    private UserController userController;
+
     @Test
     public void testCreateUser() {
+        // given
+        // when
+        long result = userController.createUser("James Bond");
 
+        // then
+        Assert.assertEquals(1, result);
     }
 
     @Test
     public void testBlockUser() {
+        // given
+        // when
+        long result = userController.blockUser(45L);
+
+        // then
+        Assert.assertEquals(45L, result);
 
     }
 
     @Test
     public void testGenerateKey() {
+        // given
+        // when
+        long key = userController.generateKey(23L);
+
+        // then
+        Assert.assertEquals(12345678L,  key);
 
     }
 
