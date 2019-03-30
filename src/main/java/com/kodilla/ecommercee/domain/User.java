b7c914ff9420ec8260ed8314cd362eb1e6e3d3f6
karolpolikarp <123@MacBook-Pro-Karol-2.local>
2019-03-30 19:20:15 +0100

    Code smell correction

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -2,7 +2,6 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
