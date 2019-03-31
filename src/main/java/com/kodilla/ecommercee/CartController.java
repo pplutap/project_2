1070564eb1a93c9d3e71504c93fd242f3e4ec692
k.dabrowski8712 <k.dabrowski8712@gmail.com>
2019-03-31 23:01:37 +0200

    task JDP190302-3 initial version

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,43 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("carts")
+
 public class CartController {
+
+    @RequestMapping( method = RequestMethod.GET, value = "getProductsFromCart")
+    public String getProductsFromCart(@RequestParam Long cartID) {
+
+        return "cloth , paper";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "addProductToCart")
+    public String addProductToCart() {
+
+        return "producr added";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createEmptyCart")
+    public String createEmptyCart() {
+
+        return "cart created";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProductFromCart")
+    public String deleteProductFromCart() {
+
+        return "product removed";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrderFromCart")
+    public String createOrderFromCart() {
+
+        return "order created";
+    }
+
 }
+
+
