307c9b0f70d32743cb61c794e9c6646805bd9937
karol-piotrowski <kp0@o2.pl>
2019-04-03 00:32:53 +0200

    User entity updated, User repository class created

--- src/main/java/com/kodilla/ecommercee/user/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/user/domain/User.java
@@ -3,15 +3,28 @@ package com.kodilla.ecommercee.user.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
+
+    @Column(name = "name")
     private String userName;
+
+    @Column(name = "status")
     private String status;
+
+    @Column(name = "userkey")
     private Long userKey;
 
 }
---
+++ src/main/java/com/kodilla/ecommercee/user/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.kodilla.ecommercee.user.repository;
+
+import com.kodilla.ecommercee.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    User save(User user);
+
+    User findByUserId(Long userId);
+
+
+}
