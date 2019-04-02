0a2f79c1a56cf3709ca79caa6dcde20a57a68314
fmatusiak <f.matusiak95@gmail.com>
2019-04-02 03:30:25 +0200

    remove import lombok in class ProductDto

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -3,16 +3,15 @@ package com.kodilla.ecommercee.domain.dto;
 import com.kodilla.ecommercee.domain.Cart;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class ProductDto {
-    private Cart cart;
     private final Long id;
     private final String name;
     private final Double price;
+    private Cart cart;
 }
 
