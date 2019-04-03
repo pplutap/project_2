13d13e07b17b44cae8db50f01e99b4ac5c87666f
nyuqiu <tomeczek110@gmail.com>
2019-04-03 05:31:34 +0100

    JDP190302-2 ready

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,30 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+    @GetMapping
+    public List<String> getGroups(){
+        return new ArrayList<>();
+    }
+
+    @GetMapping("{id}")
+    public String getGroup(@PathVariable("id") Long productId) {
+        return "get group";
+    }
+
+    @PutMapping()
+    public String updateGroup(@RequestBody String string){
+        return "update product";
+    }
+
+    @PostMapping(consumes = "application/json")
+    public void createGroup(@RequestBody String string){
+
+    }
 }
