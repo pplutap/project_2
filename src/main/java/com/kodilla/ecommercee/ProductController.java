4f312e467b650f10da5b203f5d183c7624d6096b
nyuqiu <tomeczek110@gmail.com>
2019-04-03 05:14:03 +0100

    JDP190302-1 read7

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -2,32 +2,35 @@ package com.kodilla.ecommercee;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/product")
 public class ProductController {
 
     @GetMapping
-    public List<ProductDto> getProducts(){
-
+    public List<String> getProducts(){
+        return new ArrayList<>();
     }
 
     @GetMapping("{id}")
-    public ProductDto getProduct(@PathVariable("id") Long productId) thorws ProductNotFoundException {
-
+    public String getProduct(@PathVariable("id") Long productId) {
+        return null;
     }
 
     @DeleteMapping("id")
-    public void deleteProduct(@PathVariable("id") Long taskId){
+    public void deleteProduct(@PathVariable("id") Long productId){
 
     }
 
     @PutMapping()
-    public ProductDto updateTask(@RequestBody ProductDto productDto){
-
+    public String updateTask(@RequestBody String string){
+        return null;
     }
 
     @PostMapping(consumes = "application/json")
-    public void createTask(@RequestBody ProductDto productDto){
+    public void createTask(@RequestBody String string){
 
     }
 }
