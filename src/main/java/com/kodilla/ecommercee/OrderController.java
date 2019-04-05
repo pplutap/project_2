3fdfb8413e28ef6e3d8746d8c275102301418238
Lipien <adam@pawtel.pl>
2019-04-05 10:36:07 +0200

    JDP100302-4 OrderController commit2 (zmienione adnotacje crud).

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -3,40 +3,32 @@ package com.kodilla.ecommercee;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-
 @RestController
-@RequestMapping("/ecommercee/v1/order")
+@RequestMapping("/v1/order")
 public class OrderController {
-    private List <String> orders = new ArrayList <>(Arrays.asList("order1", "order2", "order3"));
 
-
-    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    @GetMapping(value = "getOrders")
     public List <String> getOrders() {
-        return orders;
+        return new ArrayList <>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
+    @GetMapping(value = "getOrder")
     public String getOrder(@RequestParam int id) {
-        String order = orders.get(id);
-        return order;
+        return "order";
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    @DeleteMapping(value = "deleteOrder")
     public void deleteOrder(@RequestParam int id) {
-        orders.remove(id);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    @PutMapping(value = "updateOrder")
     public String updateOrder(@RequestBody String order) {
-        return orders.set(0, order);
+        return "updated order";
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "createOrder")
     public void createOrder(@RequestBody String order) {
-        orders.add(0, order);
     }
 }
