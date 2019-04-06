1b0ef06c4a458bec7ee90c0a980f94ce14898bfc
nyuqiu <tomeczek110@gmail.com>
2019-04-06 20:45:32 +0100

    Changed request mapping for groups

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/group")
+@RequestMapping("/v1/groups")
 public class GroupController {
     @GetMapping
     public List<GroupDto> getGroups(){
