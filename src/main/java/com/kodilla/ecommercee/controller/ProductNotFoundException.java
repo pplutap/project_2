7c181ee79858321ae34a04a875fd9a49833ca879
fmatusiak <f.matusiak95@gmail.com>
2019-04-06 14:01:13 +0200

    clean code smells in ProductNotFoundException

--- src/main/java/com/kodilla/ecommercee/controller/ProductNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductNotFoundException.java
@@ -1,11 +1,4 @@
 package com.kodilla.ecommercee.controller;
 
-import java.util.logging.Logger;
-
 public class ProductNotFoundException extends Exception {
-    private static final Logger LOGGER = Logger.getLogger( ProductNotFoundException .class.getName());
-
-    public void showtMessage(Long productId){
-        LOGGER.warning("Dont find product for id: " + productId);
-    }
 }
