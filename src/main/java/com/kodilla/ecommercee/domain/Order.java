28f1edc4d3766c4c464b81964e0fe5d0003bc098
karolpolikarp <123@MacBook-Pro-Karol-2.local>
2019-04-08 23:39:06 +0200

    Initial implementation of Order entity

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -2,11 +2,49 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="orders")
 public class Order {
+
     private Long id;
     private Product product;
     private int quantity;
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="product_id")
+    public Product getProduct() {
+        return product;
+    }
+
+    @Column(name = "quantity")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
---
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.dto.OrderDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderMapper {
+    public Order mapToOrder(final OrderDto orderDto) {
+        return new Order(
+                orderDto.getId(),
+                orderDto.getProduct(),
+                orderDto.getQuantity());
+    }
+
+    public OrderDto mapToOrderDto(final Order order) {
+        return new OrderDto(
+                order.getId(),
+                order.getProduct(),
+                order.getQuantity());
+    }
+
+    public List<OrderDto> mapToOrderDtoList(final List<Order> orderList) {
+        return orderList.stream()
+                .map(t -> new OrderDto(t.getId(), t.getProduct(), t.getQuantity()))
+                .collect(Collectors.toList());
+    }
+
+}
