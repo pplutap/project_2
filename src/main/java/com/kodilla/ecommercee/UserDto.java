87d4e074c83e25ee6969493bbb9c3e921ba2ccb6
PawelD1 <pawel.dz.m@interia.pl>
2019-04-09 10:10:51 +0200

    Update UserDto.java

--- src/main/java/com/kodilla/ecommercee/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/UserDto.java
@@ -1,32 +1,4 @@
 package com.kodilla.ecommercee;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class UserDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private LocalDate birthDate;
-    private String adress;
-    private String login;
-    private String password;
-    private boolean isBlocked;
-    private String uuid;
-    private LocalTime beginValidityOfUuid;
-    private LocalTime endValidityOfUuid;
-
-
 }
-
-
