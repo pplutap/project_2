a4fbbbf2642ee8dea2a9e68a806178b6ebdeffe8
nyuqiu <tomeczek110@gmail.com>
2019-04-09 15:24:04 +0100

    Added adnotation Slf4j

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,37 +1,36 @@
 package com.kodilla.ecommercee;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/groups")
 public class GroupController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GroupController.class);
 
     @GetMapping
     public List<GroupDto> getGroups(){
-        LOGGER.info("Return all groups");
+        log.info("Return all groups");
         return new ArrayList<>();
     }
 
     @GetMapping("{id}")
     public GroupDto getGroup(@PathVariable("id") Long productId) {
-        LOGGER.info("Return one group");
+        log.info("Return one group");
         return new GroupDto();
     }
 
     @PutMapping
     public GroupDto updateGroup(@RequestBody GroupDto groupDto){
-        LOGGER.info("Updated group");
+        log.info("Updated group");
         return new GroupDto();
     }
 
     @PostMapping
     public void createGroup(@RequestBody GroupDto groupDto){
-        LOGGER.info("Group is created");
+        log.info("Group is created");
     }
 }
