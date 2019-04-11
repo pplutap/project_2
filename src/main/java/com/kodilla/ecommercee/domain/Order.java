7276771a1e590a47c9728ec0910984af3fb0ae46
nyuqiu <tomeczek110@gmail.com>
2019-04-11 02:45:42 +0100

    Inital commit, classes moved to right packages

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,26 +1,27 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.domain;
 
 import lombok.AccessLevel;
 import lombok.Setter;
+
 import javax.persistence.*;
 
 @Setter(AccessLevel.PRIVATE)
 @Entity
-@Table(name="PRODUCTS")
-public class Product {
+@Table(name = "ORDERS")
+public class Order  {
 
     private Long id;
     private Cart cart;
-
-    @ManyToOne
-    @JoinColumn(name="CART_ID")
-    public Cart getCart() {
-        return cart;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
+    }
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="CART_ID")
+    public Cart getCart() {
+        return cart;
     }
 }
