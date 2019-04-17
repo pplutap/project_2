3e426be3cf82596b9d535091d9bac0438f6d48fe
Lipien <adam@pawtel.pl>
2019-04-17 05:18:26 +0200

    JDP190302-38 usunięte zbędne importy w OrderDto.

--- src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
@@ -1,13 +1,10 @@
 package com.kodilla.ecommercee.domain.dto;
 
 import com.kodilla.ecommercee.domain.Cart;
-import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
