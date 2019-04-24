a24cd1481eb0ff832a57c454a2fb2e6a446ab43c
rbarcik <barcik.r@gmail.com>
2019-04-24 23:14:36 +0200

    removed unused imports in User, Item, Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -4,7 +4,6 @@ package com.kodilla.ecommercee.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/com/kodilla/ecommercee/domain/Item.java
+++ src/main/java/com/kodilla/ecommercee/domain/Item.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
