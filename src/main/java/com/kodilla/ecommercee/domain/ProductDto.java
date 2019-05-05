e567f712bb00dce7f8e554b5d66a439d2667fc04
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-05-05 15:22:44 +0200

    Product Controller

--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -3,7 +3,7 @@ package com.kodilla.ecommercee.domain;
 public class ProductDto {
     private Long id;
     private String name;
-    private int quantity;
+    private double prize;
 
     public ProductDto() {
     }
@@ -11,7 +11,7 @@ public class ProductDto {
     public ProductDto(Long id, String name, int quantity) {
         this.id = id;
         this.name = name;
-        this.quantity = quantity;
+        this.prize = quantity;
     }
 
     public Long getId() {
@@ -22,7 +22,7 @@ public class ProductDto {
         return name;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public double getPrize() {
+        return prize;
     }
 }
