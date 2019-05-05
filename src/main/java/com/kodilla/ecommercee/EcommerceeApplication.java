004b6344e0590e657549f05756d822b2439004c3
Makedo81 <domi.stan@wp.pl>
2019-05-05 22:39:29 +0200

    JDP190401-2 adding lombok to GroupDto and corrections to application.properties

--- src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
+++ src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.dto.GroupDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -1,28 +1,12 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class GroupDto {
 
     private Long id;
     private String name;
-
-    public GroupDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 }
