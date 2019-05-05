b18532842e9f1b1623e83044c0f84f83917edda1
Makedo81 <domi.stan@wp.pl>
2019-05-05 21:50:14 +0200

    JDP190401-2 adding lombok and corrections to application.properties

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/group")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET,value ="getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value ="createGroup")
+    public GroupDto createGroup(GroupDto groupDto){
+        return new GroupDto(1L,"Ubrania");
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value ="getGroup")
+    public GroupDto getGroup(Long id) {
+        return new GroupDto(1L,"Ubrania");
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value ="updateGroup")
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return new GroupDto(1L,"Ubrania zimowe");
+    }
 }
