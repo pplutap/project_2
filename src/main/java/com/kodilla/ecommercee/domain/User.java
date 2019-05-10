3752757c02c43dd9df2e59205b257084c048cb60
Makedo81 <domi.stan@wp.pl>
2019-05-10 20:28:17 +0200

    corrections to user class,removing unwanted imports

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
