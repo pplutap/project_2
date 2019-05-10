e9689b06cdde9ba332c2ad9507e847ca30802471
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-05-10 21:31:13 +0200

    creating mapper & service packages

---
+++ src/main/java/com/kodilla/ecommercee/service/GroupService.java
@@ -0,0 +1,10 @@
+package com.kodilla.ecommercee.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class GroupService {
+}
