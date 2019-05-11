5348018b4f366579832440ddd8acb32f7dcb8cf4
golfik2005 <spiotrek1985@gmail.com>
2019-05-12 00:00:47 +0200

    - implements UserDao

--- src/main/java/com/kodilla/ecommercee/dao/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/UserDao.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee.dao;
 
-public interface UserDao {
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+    @Override
+    List<User> findAll();
+
+    @Override
+    Optional<User> findById(Long Id);
+
+    @Override
+    Product save (User user);
+
+    @Override
+    void deleteById(Long Id);
 }
