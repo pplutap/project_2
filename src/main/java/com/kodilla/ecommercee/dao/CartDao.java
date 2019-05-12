2e43bd2991650ed2686b54e33200eb2dc97bc983
Makedo81 <domi.stan@wp.pl>
2019-05-12 19:32:24 +0200

    methods save(), deleteById(), findById() implemantation

--- src/main/java/com/kodilla/ecommercee/dao/CartDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/CartDao.java
@@ -1,4 +1,22 @@
 package com.kodilla.ecommercee.dao;
 
-public interface CartDao {
+import com.kodilla.ecommercee.domain.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface CartDao extends CrudRepository<Cart,Long> {
+
+    @Override
+    Cart save (Cart cart);
+
+    @Override
+    void deleteById(Long Id);
+
+    @Override
+    Optional<Cart> findById(Long Id);
 }
+
