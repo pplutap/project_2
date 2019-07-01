e84b1ee59eee47100809f4fa4c6dc96335e6b02e
psheemas <psheemas@gmail.com>
2019-07-01 20:15:20 +0200

    JDP190602-13

--- src/main/java/com/kodilla/ecommercee/Group.java
+++ src/main/java/com/kodilla/ecommercee/Group.java
@@ -1,8 +1,41 @@
 package com.kodilla.ecommercee;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GROUP")
 public class Group {
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Getter
+    @Setter
+    @Column(name = "ID")
     private Long id;
 
+    @Getter
+    @Setter
+    @Column(name = "NAME")
     private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @Getter
+    @Setter
+    private List<Product> products = new ArrayList<>();
 }
---
+++ src/main/java/com/kodilla/ecommercee/GroupRepository.java
@@ -0,0 +1,6 @@
+package com.kodilla.ecommercee;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupRepository extends CrudRepository<Group, Long> {
+}
