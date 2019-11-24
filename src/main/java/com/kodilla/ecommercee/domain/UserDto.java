2902e3c334f30493221ad66b1b1f66d72776fb8d
JohnnyRush <pospiechjan@gmail.com>
2019-11-24 23:14:45 +0100

    JDP191101-3: adding lombok to dependencies, creating domain classes (…

---
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserDto {
+    private String  userName;
+    private Long userId;
+    private boolean isBlocked;
+}
---
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee.repository;
+
+public class UserRepository {
+}
