f41084e702e8e872153660d77d7d2c75113da77e
JohnnyRush <pospiechjan@gmail.com>
2019-11-29 20:13:04 +0100

    JDP191101-5: implementing fixes

--- src/main/java/com/kodilla/ecommercee/domain/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -1,14 +1,5 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
 public class UserDto {
-    private String userName;
-    private Long userId;
-    private boolean isBlocked;
+
 }
