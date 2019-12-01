696939251f2452eb56c432fe74d9262ed847a835
JohnnyRush <pospiechjan@gmail.com>
2019-12-01 16:19:02 +0100

    Changing Long to long, shorter method name

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -3,32 +3,32 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.UserDto;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
 
     @GetMapping("/{userId}")
-    public UserDto getUser(@PathVariable Long userId) {
+    public UserDto get(@PathVariable long userId) {
         return null;
     }
 
-    @DeleteMapping
-    public void deleteUser(@RequestParam Long userId) {
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable long userId) {
     }
 
     @PostMapping
-    public void createUser(@RequestBody UserDto userDto) {
+    public void create(@RequestBody UserDto userDto) {
     }
 
     @PutMapping("/{userId}")
-    public UserDto blockUser(@PathVariable Long userId) {
+    public UserDto blockUser(@PathVariable long userId) {
         return null;
     }
 
-    @GetMapping("/getUniqueKey")
-    public String getUniqueKey(@RequestParam Long userId, String userName) {
+    @GetMapping("/{userId}/uniqueKey")
+    public Long getUniqueKey(@PathVariable long userId, @RequestParam String username) {
         return null;
     }
 }
