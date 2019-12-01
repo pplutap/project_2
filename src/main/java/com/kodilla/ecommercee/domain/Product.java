7dd11134c50aae2c81c10675ef3b3ee0b2e65025
wojtekkazek <wojciech.kazek@gmail.com>
2019-12-01 17:28:06 +0000

    JDP191101-14: adding arguments and relations for Cart and CartDto, addding adequate relations for Product and Order to enable build

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity(name = "PRODUCTS")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "CART_ID")
+    private Cart cart;
+
 }
