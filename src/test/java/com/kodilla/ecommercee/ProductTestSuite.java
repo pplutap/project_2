bf1624147be2fa990cdf760d1c239abad5c3210a
wojtekkazek <wojciech.kazek@gmail.com>
2019-12-04 22:28:44 +0000

    TQA

--- src/test/java/com/kodilla/ecommercee/ProductTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/ProductTestSuite.java
@@ -1,41 +1,41 @@
-//package com.kodilla.ecommercee;
-//
-//import com.kodilla.ecommercee.domain.ProductDto;
-//import com.kodilla.ecommercee.domain.ProductNotFoundException;
-//import com.kodilla.ecommercee.service.ProductService;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//public class ProductTestSuite {
-//
-//    @Autowired
-//    private ProductService productService;
-//
-//    @Test
-//    public void addProductTest() throws ProductNotFoundException {
-//        //Given
-//        ProductDto product = new ProductDto();
-//
-//        //When
-//        productService.saveProduct(product);
-//
-//        //Then
-//        Assert.assertEquals(1, productService.getProducts().size());
-//    }
-//
-//    @Test
-//    public void deleteProductTest() throws ProductNotFoundException {
-//        //Given
-//        ProductDto product = new ProductDto();
-//        ProductDto product2 = new ProductDto();
-//
-//        //When
-//        productService.saveProduct(product);
-//        productService.saveProduct(product2);
-//        productService.deleteProductById(2L);
-//
-//        //Then
-//        Assert.assertEquals(1, productService.getProducts().size());
-//    }
-//}
+package com.kodilla.ecommercee;
+
+import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.domain.ProductNotFoundException;
+import com.kodilla.ecommercee.service.ProductService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductTestSuite {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void addProductTest() throws ProductNotFoundException {
+        //Given
+        ProductDto product = new ProductDto();
+
+        //When
+        productService.saveProduct(product);
+
+        //Then
+        Assert.assertEquals(1, productService.getProducts().size());
+    }
+
+    @Test
+    public void deleteProductTest() throws ProductNotFoundException {
+        //Given
+        ProductDto product = new ProductDto();
+        ProductDto product2 = new ProductDto();
+
+        //When
+        productService.saveProduct(product);
+        productService.saveProduct(product2);
+        productService.deleteProductById(2L);
+
+        //Then
+        Assert.assertEquals(1, productService.getProducts().size());
+    }
+}
