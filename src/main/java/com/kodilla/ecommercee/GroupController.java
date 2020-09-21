9257a7a5873e58b73fb72138b1921a906a2038e1
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-09-21 18:52:02 +0200

    Task ommercee/GroupController.java         modified:   src/main/resources/application.properties
    marcin@MacBook-Air-Marcin project-jdp-2009-02 % git commit -m Task

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET, value="getGroups")
+    public List<String> getGroups(){
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value="createGroup")
+    public void createGroup(){
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="getGroup")
+    public String getGroup(){
+        return "T-shirts";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="updateGroup")
+    public String updateGroup(){
+        return "T-shirt blue";
+    }
 }
