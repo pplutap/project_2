f12a830ba882de808eb45ddcb1eb29f8890cdb99
mzieniewicz <mzieniewicz@gmail.com>
2020-09-22 15:44:48 +0200

    First commit

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,4 +1,36 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    public List<String> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public void createOrder() {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
+    public String getOrder() {
+        return "Show the order";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    public String updateOrder() {
+        return "The order update";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    public void deleteOrder() {
+    }
 }
