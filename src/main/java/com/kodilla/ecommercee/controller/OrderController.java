cf1edf4906ab4d6187572d460f8cc046302e1e44
mzieniewicz <mzieniewicz@gmail.com>
2020-09-22 19:28:48 +0200

    add classes: Order, OrderDto and OrderNotFoundException, add methods in OrderController

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.OrderDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,25 +16,29 @@ import java.util.List;
 public class OrderController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrders")
-    public List<String> getGroups() {
+    public List<Order> getOrders(@RequestParam Long userId) {
         return new ArrayList<>();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createOrder")
     public void createOrder() {
+        System.out.println("The order has been created");
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrder")
-    public String getOrder() {
-        return "Show the order";
+    public OrderDto getOrder(@RequestParam Long OrderId) throws OrderNotFoundException {
+        OrderDto testOrderDto = new OrderDto(1L, 1L, 2020, 9, 22);
+        return testOrderDto;
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
-    public String updateOrder() {
-        return "The order update";
+    public OrderDto updateOrder(@RequestParam OrderDto orderDto) {
+        OrderDto testOrderDto = new OrderDto(2L, 2L, 2020, 9, 23);
+        return testOrderDto;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
-    public void deleteOrder() {
+    public void deleteOrder(@RequestParam Long OrderId) {
+        System.out.println("The order has been deleted");
     }
 }
