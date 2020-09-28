d21a3a2120a79ed6565d47fd5fb9d1a3cbdd05dc
gothamloop <g.otham@webtunnelo.com>
2020-09-28 17:22:22 +0200

    JDP200902-5_UserController

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,13 +1,12 @@
 package controller;
 
 import domain.UserDto;
-import mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-import service.DbService;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,30 +16,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1/task")
 public class UserController {
-    @Autowired
-    private DbService service;
-    @Autowired
-    private UserMapper userMapper;
 
     @RequestMapping(method = RequestMethod.POST, value = "createUser", consumes = APPLICATION_JSON_VALUE)
-    public void createUser(@RequestBody UserDto userDto) {
-        service.saveUser(userMapper.mapToUser(userDto));
+    public void createUser() {
+        System.out.println("The user has been created");;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getUser")
-    public UserDto getUser(@RequestParam Long Id) throws UserNotFoundException {
-        return userMapper.mapToUserDto(service.getUser(Id).orElseThrow(UserNotFoundException::new));
+    @RequestMapping(method = RequestMethod.GET, value = "getBlockUser")
+    public void getBlockUser(@RequestParam Long Id) throws UserNotFoundException {
+        UserDto testingUserDto = new UserDto(1L, "user1", "userDescription");
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getUserToken")
     public UserDto getUserToken(@RequestParam final Long id) throws UserNotFoundException {
-        if (id == 1) {
-            return new UserDto(1L, "user", "content");
-        }
-        else
-        {
-             throw new UserNotFoundException();
-        }
+        UserDto testingUserDto = new UserDto(1L, "user1", "userDescription");
+        return testingUserDto;
     }
 }
 
