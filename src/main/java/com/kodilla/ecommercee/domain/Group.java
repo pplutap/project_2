6acf38b4fcdb390b4c7e4e558850862b06ff83d1
mzieniewicz <mzieniewicz@gmail.com>
2020-09-30 19:29:35 +0200

    JDP200902-12 Entity Product

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "GROUP")
 public class Group {
+    @Id
     private Long id;
     private String nameGroup;
     private String description;
