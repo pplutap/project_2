35774e45a1cc8879ee71790464b574bed9f17a26
Severinum <sewerynmichota@gmail.com>
2020-12-18 12:10:32 +0000

    task-JDP201202-11 updated Id in Cart and User entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,11 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "carts")
 public class Cart {
+
+    @Id
+    private Long id;
 }
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,11 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "users")
 public class User {
+
+    @Id
+    private Long id;
 }
