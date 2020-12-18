804a4ee231594d21dd8e3dbff51a644eef6a9c94
Severinum <sewerynmichota@gmail.com>
2020-12-18 12:02:18 +0000

    task-JDP201202-11 - Encja Order implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,23 +1,52 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Entity(name = "orders")
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@RequiredArgsConstructor
 public class Order {
 
     @Id
-    @GeneratedValue
-    @Getter
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "ID", unique = true)
     private Long id;
-    @Getter
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "CART_ID")
+    private Cart cartId;
+
+    @NotNull
+    @Column(name = "VALUE")
     private Double value;
+
+    @NotNull
+    @Column(name = "STATUS")
+    private String status = "pending";
+
+    @NotNull
+    @Column(name = "DATETIME")
+    private LocalDateTime dateTime;
+
+    @NotNull
+    @Column(name = "SHIPPINGADDRESS")
+    private String shippingAddress;
+
+    public Order() {
+
+    }
+
+    public Order(@NotNull Long id, User user, Cart cartId, @NotNull Double value, @NotNull String shippingAddress) {
+        this.id = id;
+        this.user = user;
+        this.cartId = cartId;
+        this.value = value;
+        this.dateTime = LocalDateTime.now();
+        this.shippingAddress = shippingAddress;
+    }
 }
