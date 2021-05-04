e7f6cce080f6196dd630f6c156738fe6444b8231
Mariusz-Karbowski <m.karbowski@outlook.com>
2021-05-04 20:29:11 +0200

    JDP210403-13

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,7 +1,32 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PRODUCTS_GROUPS")
 public class Group {
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "GROUP_ID")
     private Long groupId;
+
+    @Column(name = "GROUP_NAME")
     private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Product> productList;
 }
