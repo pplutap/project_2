c1f8a748008eb0452a07023d66ee03e11d4b4399
Jakub Piejko <https://github.com/Kubeus14/Jakub-Piejko-kodilla-java.git>
2021-05-04 06:48:10 +0200

    ProductEntity

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,7 +1,18 @@
 package com.kodilla.ecommercee.domain;
 
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="Product")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private Long id;
@@ -9,4 +20,58 @@ public class Product {
     private String description;
     private BigDecimal price;
     private String groupId;
+    private List<Order> orders = new ArrayList<>();
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="ID",unique = true)
+    public Long getId() {
+        return id;
+    }
+    @Column(name="NAME")
+    public String getName() {
+        return name;
+    }
+    @Column(name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+    @NotNull
+    @Column(name="PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+    @Column(name="groupId")
+    public String getGroupId() {
+        return groupId;
+    }
+    @ManyToMany(cascade = CascadeType.ALL)
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
