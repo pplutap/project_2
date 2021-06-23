d00cf0147c12b90a650a3924ceb883553f408b66
Maciej Rudnicki <maciejru@amazon.com>
2021-06-23 15:11:36 +0200

    Verify failed test

---
+++ src/test/java/com/kodilla/ecommercee/WrongTest.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class WrongTest {
+
+    @Test
+    public void failedTest(){
+        fail();
+    }
+
+}
