4aeb9ba790479d6b75ef2c332f4fcc03c391d8dc
Pawel Staszewski <p.staszewski1@gmail.com>
2023-03-01 21:05:06 +0100

    JDP230301-1 poprawiony

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -4,39 +4,35 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+
 @RestController
 @RequestMapping("v1/products")
-public class ProductController {
-    private final ProductService productService;
 
-    public ProductService(ProductService productService) {
-        this.productService = productService;
-    }
+public class ProductController {
 
     @GetMapping()
-    public ResponseEntity<List<Product>> getProducts() {
-        return ResponseEntity.ok(productService.getProducts());
+    public ResponseEntity<List<Product>> getProducts(); {
+        return ResponseEntity.ok(Collections.emptyList());
     }
 
     @GetMapping(value = {productId})
     public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
-        return ResponseEntity.ok(productService.getProduct(productId));
+        return ResponseEntity.ok(null);
     }
 
     @PostMapping(value = "product", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createProduct(@RequestBody ProductDto productDto) {
-        productService.createProduct();
         return ResponseEntity.ok().build();
     }
 
     @PutMapping(value = "product", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> updateProduct(@RequestBody ProductDto productDto) {
-        return ResponseEntity.ok(productService.updateProduct(productDto));
+        return ResponseEntity.ok(null);
     }
 
     @DeleteMapping("product")
     public ResponseEntity<Void> deleteProduct(Long productId) {
-        productService.deleteProduct(productId);
         return ResponseEntity.ok().build();
     }
 }
