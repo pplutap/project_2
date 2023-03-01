05af63701da8f6a194149d46f36370cd2a605d10
Pawel Staszewski <p.staszewski1@gmail.com>
2023-03-01 21:50:30 +0100

    JDP230301-5

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/users")
 public class UserController {
+
+    @PostMapping(value = "user", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createUser(@RequestBody UserDto userDto){
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping(value = "/{id}/block")
+    public ResponseEntity<Void> blockUser(Long userId){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(value = "keygenerate")
+    public ResponseEntity<UserKeyDto> generateKey() {
+        return ResponseEntity.ok(null);
+    }
 }
