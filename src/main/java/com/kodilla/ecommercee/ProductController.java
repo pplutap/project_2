6b8c39848774e77368ca2b3a8b7f1aea7d92d33c
Pawel Staszewski <p.staszewski1@gmail.com>
2023-03-03 23:52:16 +0100

    JDP230301-1 v10

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,35 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.ProductDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RequestMapping("v1/products")
+@RestController
+@CrossOrigin("*")
 public class ProductController {
+    @GetMapping()
+    public ResponseEntity<List<ProductDTO>> getProducts() {
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+    @GetMapping(value = "{productId}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(null);
+    }
+    @PostMapping(value = "product", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createProduct(@RequestBody ProductDTO productDTO) {
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping(value = "product", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO) {
+        return ResponseEntity.ok(null);
+    }
+    @DeleteMapping(value = "{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok().build();
+    }
 }
