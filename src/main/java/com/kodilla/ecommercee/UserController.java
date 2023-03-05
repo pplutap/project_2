8d61db4821e166d8d388eb28e9d664f81622a9a4
Pawel Staszewski <p.staszewski1@gmail.com>
2023-03-05 09:40:24 +0100

    JDP230301-5 v4

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -6,19 +6,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("v1/users")
+@RestController
+@CrossOrigin("*")
 public class UserController {
-
     @PostMapping(value = "user", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDto){
+    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDTO){
         return ResponseEntity.ok().build();
     }
     @PutMapping(value = "/{id}/block")
     public ResponseEntity<Void> blockUser(Long userId){
         return ResponseEntity.ok().build();
     }
-
     @PostMapping(value = "keygenerate")
     public ResponseEntity<UserKeyDTO> generateKey() {
         return ResponseEntity.ok(null);
