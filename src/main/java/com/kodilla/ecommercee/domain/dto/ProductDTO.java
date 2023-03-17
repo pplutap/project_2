e927e9335e30727cde964847ee8d6a6da5801ee5
Staszewskip <114756001+Staszewskip@users.noreply.github.com>
2023-03-17 10:01:51 +0100

    JDP230301-21: ProductController - rzeczywista implementacja metod

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDTO.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDTO.java
@@ -1,4 +1,20 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import com.kodilla.ecommercee.domain.Group;
+import lombok.Getter;
+
+@Getter
 public class ProductDTO {
+    private Long productId;
+    private String name;
+    private double price;
+    private int quantity;
+    private Group group;
+
+    public ProductDTO(String name, double price, int quantity, Group group) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.group = group;
+    }
 }
---
+++ src/main/java/com/kodilla/ecommercee/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.exception.ProductNotFoundException;
+import com.kodilla.ecommercee.mapper.ProductMapper;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> showProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product showProduct(final long productId) throws ProductNotFoundException {
+        return productRepository.findById(productId).orElseThrow(ProductNotFoundException::new);
+    }
+
+    public Product saveProduct(final Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(final long productId) {
+        productRepository.deleteById(productId);
+    }
+
+}
