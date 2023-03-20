39ba79024a4c65b2fe904aa897e41b67a46e7528
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-03-20 21:21:51 +0100

    cart implementation

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,34 +1,24 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.Cart;
-import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.dto.CartDTO;
 import com.kodilla.ecommercee.domain.dto.OrderDTO;
 import com.kodilla.ecommercee.exception.CartNotFoundException;
 import com.kodilla.ecommercee.mapper.CartMapper;
-import com.kodilla.ecommercee.mapper.OrderMapper;
-import com.kodilla.ecommercee.repository.OrderRepository;
 import com.kodilla.ecommercee.service.CartService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/carts")
-@RequiredArgsConstructor
-@CrossOrigin("*")
 public class CartController {
 
-    private final CartMapper cartMapper;
-    private final CartService cartService;
-    private final OrderRepository orderRepository;
-    private final OrderMapper orderMapper;
+    private CartMapper cartMapper;
+    private CartService cartService;
 
 
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createCart( CartDTO cartDto){
+    @PostMapping
+    public ResponseEntity<Void> createCart(@RequestBody CartDTO cartDto){
         Cart creatCart = cartMapper.mapToCart(cartDto);
         cartService.createCart(creatCart);
         return ResponseEntity.ok().build();
@@ -36,14 +26,14 @@ public class CartController {
 
     @GetMapping(value = "{cartId}")
     public ResponseEntity<CartDTO> getCartById(@PathVariable long cartId) throws CartNotFoundException {
-        Cart cart = cartService.getCartById(cartId);
-        return ResponseEntity.ok(cartMapper.mapToCartDTO(cart));
+        Cart getCart = cartService.getCartById(cartId);
+        return ResponseEntity.ok(cartMapper.mapToCartDTO(getCart));
     }
 
     @PutMapping
     public ResponseEntity<CartDTO> updateCart(@RequestBody CartDTO cartDto){
-        Cart cart = cartMapper.mapToCart(cartDto);
-        Cart saveCart = cartService.createCart(cart);
+        Cart getCart = cartMapper.mapToCart(cartDto);
+        Cart saveCart = cartService.createCart(getCart);
         return ResponseEntity.ok(cartMapper.mapToCartDTO(saveCart));
     }
 
@@ -54,11 +44,7 @@ public class CartController {
     }
 
     @PostMapping(value = "/{cartId}/order")
-    public ResponseEntity<Void> createOrderFromCart(@RequestBody CartDTO cartDTO ,@RequestBody OrderDTO orderDTO){
-        Cart cart = cartMapper.mapToCart(cartDTO);
-        Order order = orderMapper.mapToOrder(orderDTO);
-        order.setCart(cart);
-        orderRepository.save(order);
+    public ResponseEntity<Void> createOrderFromCart(@PathVariable long cartId, @RequestBody OrderDTO orderDTO){
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -1,25 +1,40 @@
 package com.kodilla.ecommercee.service;
 
 import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.exception.CartNotFoundException;
-import com.kodilla.ecommercee.mapper.CartMapper;
 import com.kodilla.ecommercee.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
 
-    private final CartMapper cartMapper;
+    private CartRepository cartRepository;
 
-    private final CartRepository cartRepository;
-
-    public Cart saveCart(Cart cart){
-        return cartRepository.save(cart);
+    public Cart createCart(Cart cart ){
+       return cartRepository.save(cart);
     }
+
     public Cart getCartById(final long cartId) throws CartNotFoundException {
         return cartRepository.findById(cartId).orElseThrow(CartNotFoundException::new);
     }
+    public void removeFromCart(long cartId, long productId)throws CartNotFoundException{
+        Cart cart = cartRepository.findById(cartId).orElseThrow(CartNotFoundException::new);
+        cart.getProducts().stream().map(Product::getProductId)
+                .collect(Collectors.toList())
+                .remove(productId);
+        cartRepository.save(cart);
+    }
+
+
 
 }
+
+
+
+
+
